854ba07388b4322971de5c307d290953d23c3a8c
Conrado-Djugo <yuri84rn@gmail.com>
2023-09-06 20:14:04 -0300

    :tada: first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+
+const calcular = document.getElementById('calcular')
+
+
+function calculando(){
+    const peso = document.getElementById('peso').value;
+    const altura = document.getElementById('altura').value;
+    const imc = document.getElementById('imc')
+    const avimc = document.getElementById('avimc')
+    
+    if (peso !=='' && altura !== '') {
+     
+        const valorIMC = (peso / (altura * altura)).toFixed(1);
+
+        let clacificacao = '';
+
+        if (valorIMC < 18.5){
+            clacificacao = 'Você está abaixo do peso'
+        }
+        if (valorIMC < 24.9){
+            clacificacao = 'Você está com o peso idel. Parabens!!!'
+        }
+        if (valorIMC < 29.9){
+            clacificacao = 'Você está acima do peso'
+        }
+        if (valorIMC < 34.9){
+            clacificacao = 'Você está com obesidade grau 1'
+        }
+        if (valorIMC < 39.9){
+            clacificacao = 'Você está com obesidade grau 2'
+        }
+        if (valorIMC >= 40){
+            clacificacao = 'Você está com obesidade grau 3'
+        }
+
+
+
+
+
+
+
+
+        avimc.textContent = clacificacao
+        imc.textContent = valorIMC
+
+    }else{
+        avimc.textContent = 'preencha todos os campos!!!'
+    }
+
+}
+
+
+
+
+calcular.addEventListener('click', calculando)
+
